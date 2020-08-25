32e631c4f4476ac0499ec6b338c97af63f9f37ee
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-25 10:33:30 -0700

    updated HBS with BS

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded( { extended: true }))
 app.use(express.json())
 
+// you have to include these two line in order for your hbs to be rendered //
 app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
-app.get('/test', (req, res) => {
-    res.render('test')
-})
+
 app.get('/bio', (req, res) => {
     res.render('bio')
+})
+
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio')
 })
 
 app.listen(3000)
